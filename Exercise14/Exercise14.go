a04bf024d804c806f051a7ab2ef03b1d738a779b
Joel Lovely <Joellovely0717@gmail.com>
2023-11-11 17:50:17 -0600

    Finished and executable

---
+++ Exercise14/Exercise14.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x = 50
+
+const y = 60
+
+func main() {
+	fmt.Printf("The Values of x is %v and the tpes of x is %T\n", x, x)
+	fmt.Printf("The Values of y is %v and the tpes of y is %T\n", y, y)
+	z := 42
+	fmt.Printf("The Values of z is %v and the tpes of z is %T\n", z, z)
+
+}
